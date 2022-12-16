51330afb4f7510bbc36069ef2ed6a64c5662ed36
Administrator <Administrator@PC-20210727UPZY>
2022-12-16 19:54:05 +0800

    添加项目文件。

---
+++ DhcpClassClient/frmMain.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace DhcpClassClient
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.llUpdateDiscover = new System.Windows.Forms.LinkLabel();
+            this.comboDiscover = new System.Windows.Forms.ComboBox();
+            this.llRequest = new System.Windows.Forms.LinkLabel();
+            this.llDiscover = new System.Windows.Forms.LinkLabel();
+            this.comboInterface = new System.Windows.Forms.ComboBox();
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tabClient = new System.Windows.Forms.TabPage();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuMain.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tcMain.SuspendLayout();
+            this.tabClient.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(800, 25);
+            this.menuMain.TabIndex = 0;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(58, 21);
+            this.mnuFile.Text = "文件(&F)";
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(116, 22);
+            this.mnuFileExit.Text = "退出(&X)";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.llUpdateDiscover);
+            this.panel1.Controls.Add(this.comboDiscover);
+            this.panel1.Controls.Add(this.llRequest);
+            this.panel1.Controls.Add(this.llDiscover);
+            this.panel1.Controls.Add(this.comboInterface);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 57);
+            this.panel1.TabIndex = 1;
+            // 
+            // llUpdateDiscover
+            // 
+            this.llUpdateDiscover.AutoSize = true;
+            this.llUpdateDiscover.Location = new System.Drawing.Point(579, 24);
+            this.llUpdateDiscover.Name = "llUpdateDiscover";
+            this.llUpdateDiscover.Size = new System.Drawing.Size(77, 12);
+            this.llUpdateDiscover.TabIndex = 4;
+            this.llUpdateDiscover.TabStop = true;
+            this.llUpdateDiscover.Text = "更新Discover";
+            this.llUpdateDiscover.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llUpdateDiscover_LinkClicked);
+            // 
+            // comboDiscover
+            // 
+            this.comboDiscover.FormattingEnabled = true;
+            this.comboDiscover.Location = new System.Drawing.Point(350, 16);
+            this.comboDiscover.Name = "comboDiscover";
+            this.comboDiscover.Size = new System.Drawing.Size(214, 20);
+            this.comboDiscover.TabIndex = 3;
+            this.comboDiscover.SelectedIndexChanged += new System.EventHandler(this.comboDiscover_SelectedIndexChanged);
+            // 
+            // llRequest
+            // 
+            this.llRequest.AutoSize = true;
+            this.llRequest.Location = new System.Drawing.Point(662, 24);
+            this.llRequest.Name = "llRequest";
+            this.llRequest.Size = new System.Drawing.Size(71, 12);
+            this.llRequest.TabIndex = 2;
+            this.llRequest.TabStop = true;
+            this.llRequest.Text = "发送Request";
+            this.llRequest.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // llDiscover
+            // 
+            this.llDiscover.AutoSize = true;
+            this.llDiscover.Location = new System.Drawing.Point(250, 19);
+            this.llDiscover.Name = "llDiscover";
+            this.llDiscover.Size = new System.Drawing.Size(77, 12);
+            this.llDiscover.TabIndex = 1;
+            this.llDiscover.TabStop = true;
+            this.llDiscover.Text = "发送Discover";
+            this.llDiscover.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llDiscover_LinkClicked);
+            // 
+            // comboInterface
+            // 
+            this.comboInterface.FormattingEnabled = true;
+            this.comboInterface.Location = new System.Drawing.Point(12, 16);
+            this.comboInterface.Name = "comboInterface";
+            this.comboInterface.Size = new System.Drawing.Size(214, 20);
+            this.comboInterface.TabIndex = 0;
+            this.comboInterface.SelectedIndexChanged += new System.EventHandler(this.comboInterface_SelectedIndexChanged);
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.tabClient);
+            this.tcMain.Controls.Add(this.tabPage2);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 82);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(800, 368);
+            this.tcMain.TabIndex = 2;
+            // 
+            // tabClient
+            // 
+            this.tabClient.Controls.Add(this.txtLog);
+            this.tabClient.Controls.Add(this.panel2);
+            this.tabClient.Location = new System.Drawing.Point(4, 22);
+            this.tabClient.Name = "tabClient";
+            this.tabClient.Padding = new System.Windows.Forms.Padding(3);
+            this.tabClient.Size = new System.Drawing.Size(792, 342);
+            this.tabClient.TabIndex = 0;
+            this.tabClient.Text = "客户消息";
+            this.tabClient.UseVisualStyleBackColor = true;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(3, 3);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(786, 274);
+            this.txtLog.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(3, 277);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(786, 62);
+            this.panel2.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 342);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tcMain);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Name = "frmMain";
+            this.Text = "客户端";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tcMain.ResumeLayout(false);
+            this.tabClient.ResumeLayout(false);
+            this.tabClient.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tabClient;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboInterface;
+        private System.Windows.Forms.LinkLabel llDiscover;
+        private System.Windows.Forms.LinkLabel llRequest;
+        private System.Windows.Forms.ComboBox comboDiscover;
+        private System.Windows.Forms.LinkLabel llUpdateDiscover;
+    }
+}
+
---
+++ DhcpClassClient/frmMain.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using DhcpClassClient.Dhcp;
+
+namespace DhcpClassClient
+{
+    public partial class frmMain : Form
+    {
+        private NetworkInterface[] m_interfaces;
+        private NetworkInterface m_interfaceMain;
+        private Socket m_ss;
+        private bool running = true;
+        private ManualResetEvent rcvAckEvent;
+        private ISet<DhcpOffer> offers;
+        private DhcpOffer selectedOffer;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            rcvAckEvent = new ManualResetEvent(false);
+            selectedOffer = null;
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private static int ExecuteNetshCommand(string command)
+        {
+            Process proc = Process.Start(new ProcessStartInfo()
+            {
+                FileName = "netsh",
+                Arguments = command,
+                UseShellExecute = false
+            });
+            proc.WaitForExit();
+            return proc.ExitCode;
+        }
+
+        private static IPv4Packet BuildDhcpDiscoverMessage(NetworkInterface inter, ushort packetId, uint dhcpTransactionId)
+        {
+            var hardwareAddress = new HardwareAddress(HardwareAddressType.Ethernet,
+                inter.GetPhysicalAddress().GetAddressBytes());
+
+            var dhcpPacket = new DhcpPacket();
+            dhcpPacket.Operation = DhcpOperation.Request;
+            dhcpPacket.MessageType = DhcpMessageType.Discover;
+            dhcpPacket.TransactionId = dhcpTransactionId;
+            dhcpPacket.HardwareAddress = hardwareAddress;
+
+            var dhcpUdpPacket = new UdpPacket();
+            dhcpUdpPacket.SourcePort = 68;
+            dhcpUdpPacket.DestinationPort = 67;
+            dhcpUdpPacket.Payload = dhcpPacket;
+
+            var dhcpIpPacket = new IPv4Packet();
+            dhcpIpPacket.Source = IPAddress.Any;
+            dhcpIpPacket.Destination = IPAddress.Broadcast;
+            dhcpIpPacket.Identifiation = packetId;
+            dhcpIpPacket.Payload = dhcpUdpPacket;
+            return dhcpIpPacket;
+        }
+
+        private static IPv4Packet BuildDhcpRequestMessage(DhcpOffer offer, NetworkInterface inter, ushort packetId, uint dhcpTransactionId)
+        {
+            var hardwareAddress = new HardwareAddress(HardwareAddressType.Ethernet,
+                inter.GetPhysicalAddress().GetAddressBytes());
+
+            var dhcpPacket = new DhcpPacket();
+            dhcpPacket.Operation = DhcpOperation.Request;
+            dhcpPacket.MessageType = DhcpMessageType.Request;
+            dhcpPacket.TransactionId = dhcpTransactionId;
+            dhcpPacket.HardwareAddress = hardwareAddress;
+
+            dhcpPacket.Options.Add(new DhcpServerIdentifierOption(offer.DhcpServer));
+            dhcpPacket.Options.Add(new DhcpClientIdentifier(hardwareAddress));
+            dhcpPacket.Options.Add(new DhcpRequestedIpAddressOption(offer.ClientAddress));
+
+            var dhcpUdpPacket = new UdpPacket();
+            dhcpUdpPacket.SourcePort = 68;
+            dhcpUdpPacket.DestinationPort = 67;
+            dhcpUdpPacket.Payload = dhcpPacket;
+
+            var dhcpIpPacket = new IPv4Packet();
+            dhcpIpPacket.Source = IPAddress.Any;
+            dhcpIpPacket.Destination = IPAddress.Broadcast;
+            dhcpIpPacket.Identifiation = packetId;
+            dhcpIpPacket.Payload = dhcpUdpPacket;
+
+            return dhcpIpPacket;
+        }
+
+        private static IPv4Packet BuildDhcpRebindMessage(DhcpOffer offer, NetworkInterface inter, ushort packetId, uint dhcpTransactionId)
+        {
+            var hardwareAddress = new HardwareAddress(HardwareAddressType.Ethernet,
+                inter.GetPhysicalAddress().GetAddressBytes());
+
+            var dhcpPacket = new DhcpPacket();
+            dhcpPacket.Operation = DhcpOperation.Request;
+            dhcpPacket.MessageType = DhcpMessageType.Request;
+            dhcpPacket.TransactionId = dhcpTransactionId;
+            dhcpPacket.HardwareAddress = hardwareAddress;
+            dhcpPacket.ClientAddress = offer.ClientAddress;
+
+            dhcpPacket.Options.Add(new DhcpClientIdentifier(hardwareAddress));
+
+            var dhcpUdpPacket = new UdpPacket();
+            dhcpUdpPacket.SourcePort = 68;
+            dhcpUdpPacket.DestinationPort = 67;
+            dhcpUdpPacket.Payload = dhcpPacket;
+
+            var dhcpIpPacket = new IPv4Packet();
+            dhcpIpPacket.Source = offer.ClientAddress;
+            dhcpIpPacket.Destination = offer.DhcpServer;
+            dhcpIpPacket.Identifiation = packetId;
+            dhcpIpPacket.Payload = dhcpUdpPacket;
+
+            return dhcpIpPacket;
+        }
+
+        private static Socket SetupPromiscousSocket(NetworkInterface inter)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Udp);
+            IPAddress listeningAddress = inter.GetIPProperties().UnicastAddresses
+                .Where(info => info.Address.AddressFamily == AddressFamily.InterNetwork)
+                .Select(info => info.Address)
+                .First();
+            Console.WriteLine($" Binding to address {listeningAddress}");
+            socket.Bind(new IPEndPoint(listeningAddress, 0));
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, 1);
+
+            byte[] byOn = BitConverter.GetBytes(1);
+            socket.IOControl(IOControlCode.ReceiveAll, byOn, BitConverter.GetBytes(0));
+            socket.EnableBroadcast = true;
+            return socket;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            m_interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var item in m_interfaces.Select((i, idx) => new { nr = idx + 1, inter = i }))
+            {
+                //Console.WriteLine($"-{item.nr}- {item.inter.Name}");
+                comboInterface.Items.Add($"{item.nr}- {item.inter.Name}");
+            }
+        }
+
+        private void comboInterface_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Trace.WriteLine(comboInterface.SelectedIndex);
+            if (comboInterface.SelectedIndex >= 0)
+            {
+                m_interfaceMain = m_interfaces[comboInterface.SelectedIndex];
+                if (m_ss != null)
+                {
+                //    m_ss.Close();
+                }
+                //m_ss = SetupPromiscousSocket(m_interfaceMain);
+            }
+
+        }
+
+        private void Log(string strLog)
+        {
+            txtLog.Text += strLog + "\r\n";
+        }
+
+        protected void TestDiscover()
+        {
+            var random = new Random();
+            ushort packetId = (ushort)random.Next();
+            uint dhcpTransactionId = (uint)random.Next();
+
+            ITransportPacketFactory transportPacketFactory = new TransportPacketFactoryImpl();
+            IApplicationPacketFactory applicationPacketFactory = new ApplicationPacketFactoryImpl();
+
+            var dhcpDiscover = BuildDhcpDiscoverMessage(m_interfaceMain, packetId, dhcpTransactionId);
+
+            running = true;
+            bool rcvAck = false;
+            rcvAckEvent = new ManualResetEvent(false);
+            //DhcpOffer selectedOffer = null;
+            m_ss = SetupPromiscousSocket(m_interfaceMain);
+
+            Log("Send DHCP Discover...");
+            dhcpDiscover.Send(m_ss);
+
+            offers = new HashSet<DhcpOffer>();
+            var recieverThread = new Thread(() =>
+            {
+                while (running)
+                {
+                    if (m_ss == null)
+                        break;
+
+                    IPv4Packet rcvPacket = IPv4Packet.Recieve(m_ss, transportPacketFactory, applicationPacketFactory);
+
+                    if (rcvPacket != null && rcvPacket.Payload is UdpPacket udpPacket)
+                    {
+                        if (udpPacket.Payload is DhcpPacket responseDhcpPacket)
+                        {
+                            switch (responseDhcpPacket.MessageType)
+                            {
+                                case DhcpMessageType.Offer:
+                                    DhcpOffer offer = DhcpOffer.FromDhcpPacket(responseDhcpPacket);
+                                    offers.Remove(offer); // remove old offer from server if necassary
+                                    offers.Add(offer);
+                                    Trace.WriteLine($"New Offer from {rcvPacket.Source}");
+                                    break;
+                                case DhcpMessageType.Ack:
+                                    if (!rcvAck)
+                                    {
+                                        Trace.WriteLine("OK from server - ready to launch!");
+                                        if(selectedOffer==null)
+                                        {
+                                            DhcpOffer[] finalOffers = offers.ToArray();
+                                            if (finalOffers.Length != 0)
+                                                selectedOffer = finalOffers[0];
+                                            else
+                                                break;
+                                        }
+
+                                        int exitCodeInter = ExecuteNetshCommand(
+                                            $"interface ipv4 set address name=\"{m_interfaceMain.Name}\" static" +
+                                            $" {selectedOffer.ClientAddress} {selectedOffer.SubnetMask} {selectedOffer.Gateway} 1"
+                                        );
+                                        Trace.WriteLine($"netsh Interface exit code - {exitCodeInter}");
+
+                                        int exitCodeDns = ExecuteNetshCommand(
+                                            $"interface ipv4 set dnsservers \"{m_interfaceMain.Name}\" static {selectedOffer.DnsServer} primary no"
+                                        );
+                                        Trace.WriteLine($"netsh DNS exit code - {exitCodeDns}");
+                                        rcvAckEvent.Set();
+                                    }
+                                    rcvAck = true;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            });
+            recieverThread.Start();
+
+            Thread.Sleep(500);
+            if (offers.Count == 0)
+            {
+                Log("Resending DHCP Discover...");
+                dhcpDiscover.Send(m_ss);
+            }
+        }
+
+         private void llDiscover_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            TestDiscover();
+        }
+
+        private void TestRequest()
+        {
+            var random = new Random();
+            ushort packetId = (ushort)random.Next();
+            uint dhcpTransactionId = (uint)random.Next();
+
+            //Console.WriteLine();
+
+            Log("Send DHCP Request");
+            IPv4Packet dhcpRequest = BuildDhcpRequestMessage(selectedOffer, m_interfaceMain, ++packetId, dhcpTransactionId);
+            dhcpRequest.Send(m_ss);
+
+            Log("Waiting for DHCP Ack and netsh...");
+            rcvAckEvent.WaitOne();
+            running = false;
+            if (m_ss == null)
+                return;
+
+            m_ss.Close();
+            m_ss = null;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            TestRequest();
+        }
+
+        private void llUpdateDiscover_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Log("-- Select DHCP server offer --");
+            DhcpOffer[] finalOffers = offers.ToArray();
+            foreach (var item in finalOffers.Select((o, idx) => new { nr = idx + 1, offer = o }))
+            {
+                string strTemp = $"-{item.nr}- Offer from {item.offer.DhcpServer}";
+                Log(strTemp);
+                comboDiscover.Items.Add(strTemp);
+                Log($"   Client address: {item.offer.ClientAddress}");
+                Log($"   Gateway address: {item.offer.Gateway}");
+                Log($"   DNS server: {item.offer.DnsServer}");
+                Log($"   Domain: {item.offer.Domain}");
+            }
+        }
+
+        private void comboDiscover_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboDiscover.SelectedIndex < 0)
+                return;
+
+            DhcpOffer[] finalOffers = offers.ToArray();
+            int selection = comboDiscover.SelectedIndex;
+            //DhcpOffer selectedOffer = null;
+            selectedOffer = finalOffers[selection];
+
+            Trace.WriteLine("comboDiscover_SelectedIndexChanged" + selectedOffer.DhcpServer);
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (m_ss == null)
+                return;
+
+            m_ss.Close();
+        }
+    }
+}
